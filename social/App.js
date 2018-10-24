f86463c4349dc8418b2a490579fd621aa1c0d492
Gan <geekwithgun@gmail.com>
2018-10-24 08:18:09 +0800

    navigation and icons
    navigation works on both android and ios. Icon works only on ios for now

--- social/App.js
+++ social/App.js
@@ -1,49 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Navigation} from 'react-native-navigation'
+import Icon from 'react-native-vector-icons/Feather'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Explore from "./src/screens/Explore/Explore"
+import Profile from "./src/screens/Profile/Profile"
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+Navigation.registerComponent(
+  "social.ExploreScreen",
+  () => Explore
+)
+
+Navigation.registerComponent(
+  "social.ProfileScreen",
+  () => Profile
+)
+
+export default () => {
+  Promise.all([
+    Icon.getImageSource('compass',30),
+    Icon.getImageSource('user',30)
+  ]).then( sources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: 'social.ExploreScreen',
+          icon: sources[0]
+        },
+        {
+          screen: 'social.ProfileScreen',
+          icon: sources[1]
+        }
+      ]
+    })
+  })
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+// Navigation.startSingleScreenApp({
+//   screen: {
+//     screen: 'social.ExploreScreen',
+//     title: 'explore'
+//   }
+// })
